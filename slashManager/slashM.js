2e168a31c74994c18e276f8f4bf1ff67d74803c9
lion.chuang <lion.chuang@shoalter.com>
2022-01-17 18:40:35 +0800

    20220117 - bot啟動時自動註冊斜線命令 - 斜線命令相關邏輯調整

--- slashManager/slashM.js
+++ slashManager/slashM.js
@@ -9,22 +9,33 @@ const rest = new REST({ version: '9' }).setToken(auth.key);
 exports.DiscordInteraction = (async (interaction) => {
     if (!interaction.isCommand()) return;
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
+    for (i of commandDatas) {
+        if (i === null) continue;
+        if (interaction.commandName === i.name) {
+            await interaction.reply(getReply(i));
+        }
     }
 });
 
-exports.InsertSlash = (async (guildID) => {
-    try {
-        CatchF.LogDo('Started refreshing application (/) commands.');
-
-        await rest.put(
-            Routes.applicationGuildCommands(auth.botID, guildID),
-            { body: commandDatas },
-        );
-
-        CatchF.LogDo('Successfully reloaded application (/) commands.');
-    } catch (err) {
-        CatchF.EmptyDo(err);
+function getReply(interaction) {
+    switch (interaction.replyType) {
+        case 0:
+            return interaction.reply;
     }
-})();
+}
+
+exports.InsertSlash = (async (guilds) => {
+    const keys = [...guilds.keys()];
+    for (let guildID of keys) {
+        try {
+            await rest.put(
+                Routes.applicationGuildCommands(auth.botID, guildID),
+                { body: commandDatas },
+            );
+        } catch (err) {
+            CatchF.EmptyDo(err);
+        }
+    }
+});
+
+
